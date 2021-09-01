923d2de6d03dc992ea6d38d6a5bb2fa5ab38b0e7
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-01 10:58:27 +0530

    Create git.js

---
+++ plugins/git.js
@@ -0,0 +1,13 @@
+/***************************************************************************/
+/*                                                                         */
+/*  This obfuscated code was created by Javascript Obfuscator Free Version.*/
+/*  Javascript Obfuscator Free Version can be downloaded here              */
+/*  http://javascriptobfuscator.com                                        */
+/*                                                                         */
+/***************************************************************************/
+var _$_ec50=["../events","@adiwajshing/baileys","axios","../language","wallpaper","getString","git","WP","https://tinyurl.com/yjo8blfx","random","floor","arraybuffer","get","data","image","png","sendMessage","addCommand"];const Asena=require(_$_ec50[0]);const {MessageType,MessageOptions,Mimetype}=require(_$_ec50[1]);const axios=require(_$_ec50[2]);const Language=require(_$_ec50[3]);const Lang=Language[_$_ec50[5]](_$_ec50[4]);Asena[_$_ec50[17]]({pattern:_$_ec50[6],fromMe:false,desc:Lang[_$_ec50[7]]},(async (_0x229F2,_0x229B9)=>{var _0x22A2B= new Array();_0x22A2B[0]= _$_ec50[8];var _0x22980=Math[_$_ec50[10]](1* Math[_$_ec50[9]]());var _0x22A64= await axios[_$_ec50[12]](`${_0x22A2B[_0x22980]}`,{responseType:_$_ec50[11]}); await _0x229F2[_$_ec50[16]](Buffer(_0x22A64[_$_ec50[13]]),MessageType[_$_ec50[14]],{mimetype:Mimetype[_$_ec50[15]],caption:`*creater Amal*
+*owner number wa.me/919895828468*
+*bot setting video : https://youtu.be/VR_ppqEl2q8*
+*githublink       https://github.com/cyberchekuthan/Amalser_v1*
+*audio commads     https://github.com/cyberchekuthan/Amalser_v1/tree/master/uploads*
+`})}))
