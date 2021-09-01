a716e6ffbbbb209429a992bc443836a966b3f135
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-01 11:15:28 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -9,7 +9,7 @@ const Lang = Language.getString('anime');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'anime', fromMe: true, desc: Lang.AN}, (async (message, match) => {
+    Asena.addCommand({pattern: 'anime', fromMe: true, desc: 'random anime image'}, (async (message, match) => {
 
     var r_text = new Array ();
 
@@ -103,7 +103,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'anime', fromMe: false, desc: Lang.AN}, (async (message, match) => {
+    Asena.addCommand({pattern: 'anime', fromMe: false, desc: 'random anime image'}, (async (message, match) => {
 
     var r_text = new Array ();
 
